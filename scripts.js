47bfa5befff157e587b48d663da5f19cb1248a5a
Lucas <lucasgalaranidc@gmail.com>
2022-08-31 19:40:45 -0300

    index.html

---
+++ scripts.js
@@ -0,0 +1,24 @@
+const hoursHand = document.querySelector(".hand.hours");
+const minutesHand = document.querySelector(".hand.minutes");
+const secondsHand = document.querySelector(".hand.seconds");
+
+const setRotation = (element, rotationPorcentage) => {
+    element.style.setProperty("--rotation", rotationPorcentage * 360);
+};
+
+function setClock() {
+    const currentDate = new Date();
+
+    const secondsPorcentage = currentDate.getSeconds() / 60;
+    const minutesPorcentage = currentDate.getMinutes() / 60;
+    const hoursPorcentage = currentDate.getHours() / 12;
+
+
+    setRotation(secondsHand, secondsPorcentage);
+    setRotation(minutesHand, minutesPorcentage);
+    setRotation(hoursHand, hoursPorcentage);
+};
+
+setClock();
+
+setInterval(setClock, 1000);
